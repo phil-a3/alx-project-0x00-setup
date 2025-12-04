50bfdb92e81fe42e9e7aae00ca97520588178a93
Philip <adegokephilip7@gmail.com>
2025-12-04 04:43:59 +0100

    button prop

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -10,6 +10,7 @@ const Landing: React.FC = () => {
       <Card />
       <Button title="Small" styles="text-sm px-2 py-1 rounded-sm m-4" />
       <Button title="Medium" styles="text-base px-4 py-2 rounded-md m-4" />
+      <Button title="Medium" styles="text-base px-4 py-2 rounded-lg m-4" />
       <Button title="Large" styles="text-lg px-6 py-3 rounded-full m-4" />
     </div>
   )
